55fd672864f6acd845e3adbe10e3c1371be2856d
shahin009 <shaheenmis170879@gmail.com>
2024-08-15 16:33:39 +0600

    user collection create running

---
+++ index.js
@@ -0,0 +1,53 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+require('dotenv').config();
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const port = process.env.PORT || 5000;
+
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.6ypdnj9.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+async function run() {
+    try {
+        const usersCollections = client.db('Boin-shop-DB').collection('users')
+        const productCollections = client.db('Boin-shop-DB').collection('products')
+
+        app.get('/products', async (req, res) => {
+            const result = await productCollections.find().toArray();
+            res.send(result)
+        })
+
+
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+
+    }
+}
+run().catch(console.dir);
+
+
+
+app.get('/', (req, res) => {
+    res.send('boin server is running')
+})
+
+app.listen(port, () => {
+    console.log(`Boin server is running on port ${port}`)
+})
